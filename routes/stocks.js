5a2fbed1aaea05d051bd3432fe2665ea1ab358ee
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-18 12:42:49 -0400

    Handle post request and pass data to javascript var

--- routes/stocks.js
+++ routes/stocks.js
@@ -7,15 +7,17 @@ router.get('/form', function(req, res, next) {
   res.render("stocks_form");
 });
 
-router.post('/', function(req, res, next) {
-  console.log("REQ", req)
-  var symbol = "TODO"
+router.post('/dashboard', function(req, res, next) {
+  console.log("FORM DATA", req.body)
+  var symbol = req.body.symbol || "OOPS"
   console.log("STOCK SYMBOL", symbol)
 
   //var requestUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}`
   //console.log("REQUEST URL", requestUrl)
 
-  res.render("stocks_dashboard", {symbol: symbol, data: []});
+  var parsedResponse = {"Meta Data":{}, "Time Series":{}}
+
+  res.render("stocks_dashboard", {symbol: symbol, data: parsedResponse});
 });
 
 module.exports = router;
