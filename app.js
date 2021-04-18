20cdd6ad8bf7e50f009304ab9b39e9b8e806cee8
MJ Rossetti <s2t2@users.noreply.github.com>
2021-04-18 16:14:36 -0400

    WIP - flashing messages but displaying them as default divs

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session') // FLASH
+var flash = require('connect-flash'); // FLASH
 var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts'); // EJS LAYOUTS
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stocksRouter = require('./routes/stocks');
+
+var SESSION_SECRET = process.env.SESSION_SECRET || "super secret" // set this to something in production
 
 var app = express();
 
@@ -21,9 +25,21 @@ app.use(expressLayouts); // EJS LAYOUTS
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(SESSION_SECRET));
+app.use(session({
+  cookie: { maxAge: 60000},
+  secret: SESSION_SECRET,
+  name: 'stocks-app-session',
+  resave: true,
+  saveUninitialized: true
+})); // FLASH
+app.use(flash()); // FLASH
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+}); // FLASH
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stocks', stocksRouter);
